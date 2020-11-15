77668e7a1af68ed916176bb4849734925563de58
Jay Salidwong <wansanohj@gmail.com>
2020-11-15 14:00:21 +0700

    create-structure-project

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+export const Header: React.FC = () => {
+    return (
+        <div>Header</div>
+    )
+}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import { Header } from '../components/Header'
+import { Introduction } from '../containers/Home'
+
+export const Home: React.FC = () => {
+    return (
+        <>
+        <Header />
+        <Introduction />
+        </>
+    )
+}
